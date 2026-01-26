4083bf43e19ee9be1fd9f251c9478c99d457b4cb
boos88978857-svg <boos88978857@gmail.com>
2026-01-26 22:03:18 +0700

    更新 page.tsx

--- app/practice/wrong/page.tsx
+++ app/practice/wrong/page.tsx
@@ -1,11 +1,5 @@
-// app/practice/wrong/page.tsx
-import React, { Suspense } from "react";
 import WrongClient from "./wrong-client";
 
-export default function WrongPage() {
-  return (
-    <Suspense fallback={<div style={{ padding: 16 }}>讀取中…</div>}>
-      <WrongClient />
-    </Suspense>
-  );
+export default function Page() {
+  return <WrongClient />;
 }
